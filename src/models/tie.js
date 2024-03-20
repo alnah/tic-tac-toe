cac4e79790c1b5feb5e62157162dcad93ca6eadf
alnah <alexis.nahan@gmail.com>
2024-03-20 16:10:29 -0300

    refactgor: change tie module to improve readability and add documentation

--- src/models/tie.js
+++ src/models/tie.js
@@ -1,7 +1,13 @@
-import {
-  validateIsBoolean,
-  validateIsPositiveInteger,
-} from "../utils/validation";
+import { validateIsBoolean, validateScore } from "../utils/validation";
+
+/**
+ * @module Tie
+ * This module provides functionality to create and manage a tie object
+ * for a game. It allows for the initialization of the tie object with
+ * customizable or default properties including score and isTie status.
+ * It offers methods to interact with the tie object such as getting its
+ * score, checking if it's a tie, and resetting its state.
+ */
 
 /**
  * Initial score value for a tie object.
@@ -14,63 +20,86 @@ const INIT_SCORE = 0;
 const INIT_TIE = false;
 
 /**
- * Factory function to create a tie object.
- * @param {Object} tie - The tie object with score and isTie properties.
- * @returns {Object} A frozen tie object with various methods for managing ties.
+ * Factory function to create a tie object. This function initializes a
+ * tie with default properties including score and isTie status. It returns
+ * an object that provides methods to interact with the tie, such as getting
+ * its score, checking if it's a tie, and resetting its state.
+ *
+ * @param {Object} [tie={ score: INIT_SCORE, isTie: INIT_TIE }] - An
+ * optional parameter used here to provide a fluent interface. Defaults are
+ * set to a score of 0 and isTie status of false.
+ * @param {number} [tie.score=0] - The score of the tie object.
+ * @param {boolean} [tie.isTie=false] - The isTie status of the tie object.
+ * @returns {Object} An object representing the tie, including methods for
+ * interacting with the tie:
+ *  - `getTie(): {Object}` Returns the tie object.
+ *  - `getScore(): {number}` Returns the score of the tie object.
+ *  - `getIsTie(): {boolean}` Returns the isTie status of the tie object.
+ *  - `setIsTie(): {Object}` Sets the isTie status to true and returns a
+ * new tie object.
+ *  - `incrementScore(): {Object}` Increments the score by 1 and returns a
+ * new tie object.
+ *  - `resetScore(): {Object}` Resets the score to 0 and returns a new tie
+ * object.
+ *  - `resetIsTie(): {Object}` Resets the isTie status to false and returns
+ * a new tie object.
+ *  - `resetTie(): {Object}` Resets both score and isTie status to their
+ * initial values and returns a new tie object.
  */
 const makeTie = (tie = { score: INIT_SCORE, isTie: INIT_TIE }) => {
   const { score, isTie } = tie;
 
-  validateIsPositiveInteger(score);
+  validateScore(score);
   validateIsBoolean(isTie);
 
   return Object.freeze({
     /**
-     * Get the tie object.
+     * Retrieves the tie object.
      * @returns {Object} The tie object.
      */
     getTie: () => tie,
 
     /**
-     * Get the score of the tie object.
+     * Retrieves the score of the tie object.
      * @returns {number} The score of the tie object.
      */
     getScore: () => score,
 
     /**
-     * Get the isTie value of the tie object.
-     * @returns {boolean} The isTie value of the tie object.
+     * Retrieves the isTie status of the tie object.
+     * @returns {boolean} The isTie status of the tie object.
      */
     getIsTie: () => isTie,
 
     /**
-     * Set the isTie value to true.
+     * Sets the isTie status to true and returns a new tie object.
      * @returns {Object} A new tie object with isTie set to true.
      */
     setIsTie: () => makeTie({ ...tie, isTie: true }),
 
     /**
-     * Increment the score of the tie object by 1.
-     * @returns {Object} A new tie object with the score incremented by 1.
+     * Increments the score by 1 and returns a new tie object.
+     * @returns {Object} A new tie object with the incremented score.
      */
     incrementScore: () => makeTie({ ...tie, score: score + 1 }),
 
     /**
-     * Reset the score of the tie object to 0.
+     * Resets the score to 0 and returns a new tie object.
      * @returns {Object} A new tie object with the score reset to 0.
      */
     resetScore: () => makeTie({ ...tie, score: 0 }),
 
     /**
-     * Reset the isTie value of the tie object to false.
-     * @returns {Object} A new tie object with isTie reset to false.
+     * Resets the isTie status to false and returns a new tie object.
+     * @returns {Object} A new tie object with isTie set to false.
      */
     resetIsTie: () => makeTie({ ...tie, isTie: false }),
 
     /**
-     * Reset the tie object to its initial state.
-     * @returns {Object} A new tie object with score and isTie reset to initial
-     * values.
+     * Resets both score and isTie status to their initial values and returns a
+     * new tie object.
+     * @returns {Object} A new tie object with score and isTie status reset to
+     * initial values.
      */
     resetTie: () => makeTie({ score: INIT_SCORE, isTie: INIT_TIE }),
   });
