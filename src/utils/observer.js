95b4763a1f0fb5c46c96bfd6aafa39fbe98857a4
alnah <alexis.nahan@gmail.com>
2024-03-20 16:26:37 -0300

    doc: impprove documentation for observer.js

--- src/utils/observer.js
+++ src/utils/observer.js
@@ -1,4 +1,13 @@
 /**
+ * @module Observer
+ * This module provides functionality to create and manage an observer pattern.
+ * It allows for the creation of an observer object with methods to add, remove,
+ * and notify observers. Observers can be functions or objects with an `update`
+ * method. This pattern is useful for implementing a publish-subscribe model,
+ * where observers are notified of changes in the subject they are observing.
+ */
+
+/**
  * Creates an observer object with methods to manage observers.
  * @returns {Object} An observer object with methods to add, remove, and notify
  * observers.
@@ -8,19 +17,24 @@ const makeObserver = () => {
 
   return Object.freeze({
     /**
-     * Add an observer to the list of observers.
-     * @param {Function} observer - The observer function to add.
+     * Adds an observer to the list of observers.
+     * @param {Function|Object} observer - The observer to add. Can be a function
+     * or an object with an `update` method.
      */
     addObserver: observer => observers.push(observer),
 
     /**
-     * Remove an observer from the list of observers.
-     * @param {Function} observer - The observer function to remove.
+     * Removes an observer from the list of observers.
+     * @param {Function|Object} observer - The observer to remove. Can be a
+     * function or an object with an `update` method.
+     * @returns {Array} The updated list of observers after removal.
      */
     removeObserver: observer => observers.filter(o => o !== observer),
 
     /**
-     * Notify all observers with the provided data.
+     * Notifies all observers with the provided data. If the observer is a
+     * function, it is called directly. If the observer is an object, its
+     * `update` method is called.
      * @param {*} data - The data to be passed to the observers.
      */
     notifyObservers: data =>
